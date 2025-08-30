b70d39b8d2acf96fbd1f6eeaaadd8f4dd3a06c32
Noah Chin-Shue <noah.chinshue@student.northampton.edu>
2025-08-30 03:21:01 +0000

    Assignment Completed

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,19 @@
-// YOUR NAME HERE
+// Noah Chin-Shue
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+    int num1, num2;
 
+    cout << "Enter first integer: ";
+    cin >> num1;
+
+    cout << "Enter second integer: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+
+    cout << "\nThe sum is " << sum << "." << endl;
+
+    return 0;
 }
